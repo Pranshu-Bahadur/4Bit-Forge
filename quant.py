b3c988c08eb0f7c8967231285732be0ef942e5e5
Pranshu Bahadur <pranshubahadur@gmail.com>
2026-01-01 04:29:46 +0800

    testing v0 e2e quantization

--- quant.py
+++ quant.py
@@ -14,7 +14,7 @@ import forge.utils
 
 from forge.gptq import GPTQ
 
-ROUTED_EXPERTS_REGEX = r".*mlp\.experts\.\d+\.(down|gate|up|gate_up)_proj$"
+ROUTED_EXPERTS_REGEX = r".*\.experts\.\d+\.(down|gate|up|gate_up)_proj$"
 
 def parse_args():
     parser = argparse.ArgumentParser()
