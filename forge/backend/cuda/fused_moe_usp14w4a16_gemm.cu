5c6aad3f7fe1c1bc169bce7b4f3ecece385d2ac7
Pranshu Bahadur <pranshubahadur@gmail.com>
2026-01-29 08:09:02 +0800

    K2 (Gate+Up) Pipelining Fixed; K4(Down). testing

--- forge/backend/cuda/fused_moe_usp14w4a16_gemm.cu
+++ forge/backend/cuda/fused_moe_usp14w4a16_gemm.cu
@@ -86,15 +86,16 @@ __device__ __forceinline__ uint32_t bf16x2_from_packed_i8pair(uint16_t packed_u)
     int8_t v0 = (int8_t)(packed_u & 0xFFu);
     int8_t v1 = (int8_t)((packed_u >> 8) & 0xFFu);
 
-    /*
+    
     uint16_t b0 = bf16bits_from_i8_small(v0);
     uint16_t b1 = bf16bits_from_i8_small(v1);
     return (uint32_t)b0 | ((uint32_t)b1 << 16);
-    */
 
+    /*
     __nv_bfloat16 b0 = __float2bfloat16((float)v0);
     __nv_bfloat16 b1 = __float2bfloat16((float)v1);
     return ((uint32_t)(*(uint16_t*)&b0)) | ((uint32_t)(*(uint16_t*)&b1) << 16);
+    */
 
 }
 
@@ -535,14 +536,14 @@ __global__ void phantom_usp14_w4a16_sym_sm80_fmoe_w13AS_mm_phase(
         ldsmB(&XS[(((int64_t)0 << 6) + ((int64_t)0 << 5)) * NTOK], bh0);
         ldsmB(&XS[(((int64_t)0 << 6) + ((int64_t)1 << 5)) * NTOK], bh1);
 
-        bf16x2x2_from_i8x4(gate.top_h0, gate_ah0.x, gate_ah0.y);
-        bf16x2x2_from_i8x4(gate.bot_h0, gate_ah0.z, gate_ah0.w);
-        bf16x2x2_from_i8x4(gate.top_h1, gate_ah1.x, gate_ah1.y);
-        bf16x2x2_from_i8x4(gate.bot_h1, gate_ah1.z, gate_ah1.w);
-        bf16x2x2_from_i8x4(up.top_h0, up_ah0.x, up_ah0.y);
-        bf16x2x2_from_i8x4(up.bot_h0, up_ah0.z, up_ah0.w);
-        bf16x2x2_from_i8x4(up.top_h1, up_ah1.x, up_ah1.y);
-        bf16x2x2_from_i8x4(up.bot_h1, up_ah1.z, up_ah1.w);
+        bf16x2x2_from_i8x4(gate.top_h0, gate_ah0.x, gate_ah0.z);
+        bf16x2x2_from_i8x4(gate.bot_h0, gate_ah0.y, gate_ah0.w);
+        bf16x2x2_from_i8x4(gate.top_h1, gate_ah1.x, gate_ah1.z);
+        bf16x2x2_from_i8x4(gate.bot_h1, gate_ah1.y, gate_ah1.w);
+        bf16x2x2_from_i8x4(up.top_h0, up_ah0.x, up_ah0.z);
+        bf16x2x2_from_i8x4(up.bot_h0, up_ah0.y, up_ah0.w);
+        bf16x2x2_from_i8x4(up.top_h1, up_ah1.x, up_ah1.z);
+        bf16x2x2_from_i8x4(up.bot_h1, up_ah1.y, up_ah1.w);
         
         for (int64_t g2 = 1; g2 < G2+1; ++g2) {
 
@@ -621,15 +622,14 @@ __global__ void phantom_usp14_w4a16_sym_sm80_fmoe_w13AS_mm_phase(
                 fscales_up.y = bf16_bits_to_f32(scales_up.y);
                 fscales_up.z = bf16_bits_to_f32(scales_up.z);
                 fscales_up.w = bf16_bits_to_f32(scales_up.w);
-
-                bf16x2x2_from_i8x4(gate.top_h0, gate_ah0.x, gate_ah0.y);
-                bf16x2x2_from_i8x4(gate.bot_h0, gate_ah0.z, gate_ah0.w);
-                bf16x2x2_from_i8x4(gate.top_h1, gate_ah1.x, gate_ah1.y);
-                bf16x2x2_from_i8x4(gate.bot_h1, gate_ah1.z, gate_ah1.w);
-                bf16x2x2_from_i8x4(up.top_h0, up_ah0.x, up_ah0.y);
-                bf16x2x2_from_i8x4(up.bot_h0, up_ah0.z, up_ah0.w);
-                bf16x2x2_from_i8x4(up.top_h1, up_ah1.x, up_ah1.y);
-                bf16x2x2_from_i8x4(up.bot_h1, up_ah1.z, up_ah1.w);
+                bf16x2x2_from_i8x4(gate.top_h0, gate_ah0.x, gate_ah0.z);
+                bf16x2x2_from_i8x4(gate.bot_h0, gate_ah0.y, gate_ah0.w);
+                bf16x2x2_from_i8x4(gate.top_h1, gate_ah1.x, gate_ah1.z);
+                bf16x2x2_from_i8x4(gate.bot_h1, gate_ah1.y, gate_ah1.w);
+                bf16x2x2_from_i8x4(up.top_h0, up_ah0.x, up_ah0.z);
+                bf16x2x2_from_i8x4(up.bot_h0, up_ah0.y, up_ah0.w);
+                bf16x2x2_from_i8x4(up.top_h1, up_ah1.x, up_ah1.z);
+                bf16x2x2_from_i8x4(up.bot_h1, up_ah1.y, up_ah1.w);
             }
         }
 
@@ -710,10 +710,10 @@ __global__ void phantom_usp14_w4a16_sym_sm80_fmoe_w2AS_mm(
     ldsmB(&XS[(((int64_t)0 << 6) + ((int64_t)0 << 5)) * NTOK], bh0);
     ldsmB(&XS[(((int64_t)0 << 6) + ((int64_t)1 << 5)) * NTOK], bh1);
 
-    bf16x2x2_from_i8x4(out.top_h0, out_ah0.x, out_ah0.y);
-    bf16x2x2_from_i8x4(out.bot_h0, out_ah0.z, out_ah0.w);
-    bf16x2x2_from_i8x4(out.top_h1, out_ah1.x, out_ah1.y);
-    bf16x2x2_from_i8x4(out.bot_h1, out_ah1.z, out_ah1.w);
+    bf16x2x2_from_i8x4(out.top_h0, out_ah0.x, out_ah0.z);
+    bf16x2x2_from_i8x4(out.bot_h0, out_ah0.y, out_ah0.w);
+    bf16x2x2_from_i8x4(out.top_h1, out_ah1.x, out_ah1.z);
+    bf16x2x2_from_i8x4(out.bot_h1, out_ah1.y, out_ah1.w);
         
     for (int64_t g2 = 1; g2 < G2+1; ++g2) {
 
@@ -761,10 +761,10 @@ __global__ void phantom_usp14_w4a16_sym_sm80_fmoe_w2AS_mm(
                 metadata_out0 = park_h0(out, (int)t);
                 metadata_out1 = park_h1(out, (int)t);
 
-                bf16x2x2_from_i8x4(out.top_h0, out_ah0.x, out_ah0.y);
-                bf16x2x2_from_i8x4(out.bot_h0, out_ah0.z, out_ah0.w);
-                bf16x2x2_from_i8x4(out.top_h1, out_ah1.x, out_ah1.y);
-                bf16x2x2_from_i8x4(out.bot_h1, out_ah1.z, out_ah1.w);
+                bf16x2x2_from_i8x4(out.top_h0, out_ah0.x, out_ah0.z);
+                bf16x2x2_from_i8x4(out.bot_h0, out_ah0.y, out_ah0.w);
+                bf16x2x2_from_i8x4(out.top_h1, out_ah1.x, out_ah1.z);
+                bf16x2x2_from_i8x4(out.bot_h1, out_ah1.y, out_ah1.w);
             }
     }
     store(Y, R, m_base, m_end, (int)groupID, (int)t, oc_base, D);
