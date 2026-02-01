bbbceb509718058bf49ab81a6c81d5404283421d
Pranshu Bahadur <pranshubahadur@gmail.com>
2026-02-01 20:55:09 +0800

    K2 (Gate+Up) Pipelining Fixed; K4(Down). testing; wip - saving state; converging; no interleaving. save state

--- forge/backend/cuda/fused_moe_usp14w4a16_gemm.cu
+++ forge/backend/cuda/fused_moe_usp14w4a16_gemm.cu
@@ -419,8 +419,8 @@ template<int F>
 __device__ __forceinline__ void mma(const __nv_bfloat162* frag_a, const __nv_bfloat162* frag_b, const uint32_t e, float4& frag_c) {
   //& @ frag_a, frag_b -> inf/nan?
 
-  const uint32_t* a = reinterpret_cast<const uint32_t*>(frag_a);
-  const uint32_t* b = reinterpret_cast<const uint32_t*>(frag_b);
+  const uint32_t* a = reinterpret_cast<const uint32_t*>(&frag_a);
+  const uint32_t* b = reinterpret_cast<const uint32_t*>(&frag_b);
 
   float* c = reinterpret_cast<float*>(&frag_c);
   
