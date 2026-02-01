325cecf3a6ce65d7de90d8919dfc7386740c6e53
Pranshu Bahadur <pranshubahadur@gmail.com>
2026-02-02 06:31:49 +0800

    K2 (Gate+Up) Pipelining Fixed; K4(Down). testing; figured out correct A mapping. save state;

--- forge/backend/cuda/fused_moe_usp14w4a16_gemm.cu
+++ forge/backend/cuda/fused_moe_usp14w4a16_gemm.cu
@@ -545,7 +545,7 @@ __global__ void phantom_usp14_w4a16_sym_sm80_fmoe_w13AS_mm_phase(
     
     const int64_t tid = (int64_t)threadIdx.x;
 
-    __shared__ __nv_bfloat16* XS;
+    extern __shared__ __nv_bfloat16 XS[];
     
     for (int64_t c = (int64_t)threadIdx.x; c < C; c += (int64_t)blockDim.x) {
         #pragma unroll NTOK
@@ -775,7 +775,7 @@ __global__ void phantom_usp14_w4a16_sym_sm80_fmoe_w2AS_mm(
     
     const int64_t tid = (int64_t)threadIdx.x;
 
-    __shared__ __nv_bfloat16*XS;
+    extern __shared__ __nv_bfloat16 XS[];
     
     for (int64_t c = tid; c < C; c += (int64_t)blockDim.x) {
         #pragma unroll NTOK
