8f6c92cebc5037cc3e4ace4568497ca8bf3a9485
Pranshu Bahadur <pranshubahadur@gmail.com>
2026-02-01 23:55:22 +0800

    K2 (Gate+Up) Pipelining Fixed; K4(Down). testing; wip - saving state; converging; interleaving a. save state;

--- forge/backend/cuda/fused_moe_usp14w4a16_gemm.cu
+++ forge/backend/cuda/fused_moe_usp14w4a16_gemm.cu
@@ -236,10 +236,10 @@ __device__ __forceinline__ void stage_decode(
     out.top_h1 = pack_i8x4_from_i16x2(top.z, top.w);
     out.bot_h1 = pack_i8x4_from_i16x2(bot.z, bot.w);
 
-    out.nib_h0_lo = pack_nib2(meta_nib_top.x, meta_nib_bot.x); // 0...3
-    out.nib_h0_hi = pack_nib2(meta_nib_top.y, meta_nib_bot.y); // 4...7
-    out.nib_h1_lo = pack_nib2(meta_nib_top.z, meta_nib_bot.z);
-    out.nib_h1_hi = pack_nib2(meta_nib_top.w, meta_nib_bot.w);
+    out.nib_h0_lo = pack_nib2(meta_nib_top.x, meta_nib_top.y); // 0...3
+    out.nib_h0_hi = pack_nib2(meta_nib_bot.x, meta_nib_bot.y); // 4...7
+    out.nib_h1_lo = pack_nib2(meta_nib_top.z, meta_nib_top.w);
+    out.nib_h1_hi = pack_nib2(meta_nib_bot.z, meta_nib_bot.w);
 }
 
 
