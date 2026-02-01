02179af2b1ec3e174d8572632508b5a15dae465a
Pranshu Bahadur <pranshubahadur@gmail.com>
2026-02-02 05:24:11 +0800

    K2 (Gate+Up) Pipelining Fixed; K4(Down). testing; wip - saving state; converging; interleaving a. save state;

--- forge/backend/cuda/fused_moe_usp14w4a16_gemm.cu
+++ forge/backend/cuda/fused_moe_usp14w4a16_gemm.cu
@@ -405,7 +405,7 @@ __device__ __forceinline__ void ldsmB(
     const void* XS_ptr,
     __nv_bfloat162* frag_b
 ) {
-    uint32_t* b = reinterpret_cast<uint32_t*>(frag_b);
+    uint32_t* b = reinterpret_cast<uint32_t*>(&frag_b);
     const uint32_t smem = static_cast<uint32_t>(__cvta_generic_to_shared(XS_ptr));
 
     asm volatile(
@@ -423,8 +423,8 @@ __device__ __forceinline__ void ldsmB(
 template<int F>
 __device__ __forceinline__ void mma(const __nv_bfloat162* frag_a, const __nv_bfloat162* frag_b, const uint32_t metadata, float4& frag_c) {
 
-  const uint32_t* a = reinterpret_cast<const uint32_t*>(frag_a);
-  const uint32_t* b = reinterpret_cast<const uint32_t*>(frag_b);
+  const uint32_t* a = reinterpret_cast<const uint32_t*>(&frag_a);
+  const uint32_t* b = reinterpret_cast<const uint32_t*>(&frag_b);
 
   float* c = reinterpret_cast<float*>(&frag_c);
   
